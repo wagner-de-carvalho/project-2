89468a8326a9e37a82251051b38f84e471add5ef
mindlab <wagncarv@outlook.com>
2023-09-06 18:08:15 -0300

    useCallBack react hook

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 // import logo from './logo.svg';
+import P from 'prop-types';
 import './App.css';
-import { useState, useEffect } from 'react';
+import React, { useCallback, useState } from 'react';
 
-const eventFn = () => {
-  console.log('h1 clicado');
+const Button = React.memo(function Button({ incrementButton }) {
+  return <button onClick={() => incrementButton(10)}>+</button>;
+});
+
+Button.propTypes = {
+  incrementButton: P.func,
 };
 
 function App() {
   const [counter, setCounter] = useState(0);
-  const [counter2, setCounter2] = useState(0);
+  const incrementCounter = useCallback((num) => {
+    setCounter((c) => c + num);
+  }, []);
+  // const [counter2, setCounter2] = useState(0);
 
   // const [reverse, setReverse] = useState(false);
   // const [counter, setCounter] = useState(0);
@@ -41,33 +49,41 @@ function App() {
   // );
 
   // ComponentDidUpdate - atualiza toda vez que o componente atualiza
-  useEffect(() => {
-    console.log('ComponentDidUpdate');
-  });
+  // useEffect(() => {
+  //   console.log('ComponentDidUpdate');
+  // });
 
   // ComponentDidMount - executa uma vez
-  useEffect(() => {
-    document.querySelector('h1')?.addEventListener('click', eventFn);
-    return () => {
-      // ComponentWillAmount - limpeza
-      document.querySelector('h1')?.removeEventListener('click', eventFn);
-    };
-  }, []);
+  // useEffect(() => {
+  //   document.querySelector('h1')?.addEventListener('click', eventFn);
+  //   return () => {
+  //     // ComponentWillAmount - limpeza
+  //     document.querySelector('h1')?.removeEventListener('click', eventFn);
+  //   };
+  // }, []);
 
   // ComponentDidMount - executa toda vez que dependência mudar
-  useEffect(() => {
-    console.log('C1: ', counter, 'C2:', counter2);
-  }, [counter, counter2]);
+  // useEffect(() => {
+  //   console.log('C1: ', counter, 'C2:', counter2);
+  // }, [counter, counter2]);
+
+  // return (
+  //   <div>
+  //     <p>
+  //       C1: {counter}
+  //       C2: {counter2}
+  //     </p>
+  //     <h1>Contador: {counter}</h1>
+  //     <button onClick={() => setCounter(counter + 1)}>+</button>
+  //     <button onClick={() => setCounter2(counter2 + 1)}>+[2]</button>
+  //   </div>
+  // );
 
   return (
     <div>
-      <p>
-        C1: {counter}
-        C2: {counter2}
-      </p>
-      <h1>Contador: {counter}</h1>
-      <button onClick={() => setCounter(counter + 1)}>+</button>
-      <button onClick={() => setCounter2(counter2 + 1)}>+[2]</button>
+      <p>Teste</p>
+      <h1>C1: {counter}</h1>
+      <Button incrementButton={incrementCounter}>+</Button>
     </div>
   );
 }
