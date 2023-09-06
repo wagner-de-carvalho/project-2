a13cf998c002c4bfd79060390b010caef4b51935
wagner <wagncarv@outlook.com>
2023-09-06 15:06:10 -0300

    ciclo de vida, use effect parte 1

--- src/App.js
+++ src/App.js
@@ -1,35 +1,53 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
-  const [reverse, setReverse] = useState(false);
   const [counter, setCounter] = useState(0);
-  const reverseClass = reverse ? 'reverse' : '';
+  // const [reverse, setReverse] = useState(false);
+  // const [counter, setCounter] = useState(0);
+  // const reverseClass = reverse ? 'reverse' : '';
 
-  const handleClick = () => {
-    setReverse(!reverse);
-  };
+  // const handleClick = () => {
+  //   setReverse((reverse) => !reverse);
+  // };
 
-  const handleIncrement = () => {
-    setCounter(counter + 1);
-  };
+  // const handleIncrement = () => {
+  //   setCounter((prevCounter) => prevCounter + 1);
+  // };
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className={`App-logo ${reverseClass}`} alt="logo" />
+  //       <h2>Contador: {counter} </h2>
+  //       <p>
+  //         <button onClick={handleClick} type="button">
+  //           Reverse
+  //         </button>
+  //       </p>
+  //       <p>
+  //         <button onClick={handleIncrement} type="button">
+  //           Increment
+  //         </button>
+  //       </p>
+  //     </header>
+  //   </div>
+  // );
+
+  // ComponentDidUpdate - atualiza toda vez que o componente atualiza
+  useEffect(() => {
+    console.log('ComponentDidUpdate');
+  });
+
+  // ComponentDidUpdate - executa uma vez
+  useEffect(() => {
+    console.log('ComponentDidMount');
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className={`App-logo ${reverseClass}`} alt="logo" />
-        <h2>Contador: {counter} </h2>
-        <p>
-          <button onClick={handleClick} type="button">
-            Reverse
-          </button>
-        </p>
-        <p>
-          <button onClick={handleIncrement} type="button">
-            Increment
-          </button>
-        </p>
-      </header>
+    <div>
+      <h1>Contador: {counter}</h1>
+      <button onClick={() => setCounter(counter + 1)}>+</button>
     </div>
   );
 }
