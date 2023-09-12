8e53e2c0e4550b1d9d954fe4095f59322a871fea
mindlab <wagncarv@outlook.com>
2023-09-12 00:07:46 -0300

    Exemplo de uso hook useMemo

--- src/App.js
+++ src/App.js
@@ -1,89 +1,58 @@
-// import logo from './logo.svg';
 import P from 'prop-types';
+import { useEffect, useMemo, useState } from 'react';
 import './App.css';
-import React, { useCallback, useState } from 'react';
 
-const Button = React.memo(function Button({ incrementButton }) {
-  return <button onClick={() => incrementButton(10)}>+</button>;
-});
+const Post = ({ post }) => {
+  console.log('Filho renderizou');
+  return (
+    <div key={post.id} className="post">
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+    </div>
+  );
+};
 
-Button.propTypes = {
-  incrementButton: P.func,
+Post.propTypes = {
+  post: P.shape({
+    id: P.number,
+    title: P.string,
+    body: P.string,
+  }),
 };
 
 function App() {
-  const [counter, setCounter] = useState(0);
-  const incrementCounter = useCallback((num) => {
-    setCounter((c) => c + num);
+  const [posts, setPosts] = useState([]);
+  const [value, setValue] = useState('');
+
+  console.log('Pai renderizou!');
+
+  // Component did mount
+  useEffect(() => {
+    setTimeout(function () {
+      fetch('https://jsonplaceholder.typicode.com/posts')
+        .then((r) => r.json())
+        .then((r) => setPosts(r));
+    }, 5000);
   }, []);
-  // const [counter2, setCounter2] = useState(0);
-
-  // const [reverse, setReverse] = useState(false);
-  // const [counter, setCounter] = useState(0);
-  // const reverseClass = reverse ? 'reverse' : '';
-
-  // const handleClick = () => {
-  //   setReverse((reverse) => !reverse);
-  // };
-
-  // const handleIncrement = () => {
-  //   setCounter((prevCounter) => prevCounter + 1);
-  // };
-  // return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <img src={logo} className={`App-logo ${reverseClass}`} alt="logo" />
-  //       <h2>Contador: {counter} </h2>
-  //       <p>
-  //         <button onClick={handleClick} type="button">
-  //           Reverse
-  //         </button>
-  //       </p>
-  //       <p>
-  //         <button onClick={handleIncrement} type="button">
-  //           Increment
-  //         </button>
-  //       </p>
-  //     </header>
-  //   </div>
-  // );
-
-  // ComponentDidUpdate - atualiza toda vez que o componente atualiza
-  // useEffect(() => {
-  //   console.log('ComponentDidUpdate');
-  // });
-
-  // ComponentDidMount - executa uma vez
-  // useEffect(() => {
-  //   document.querySelector('h1')?.addEventListener('click', eventFn);
-  //   return () => {
-  //     // ComponentWillAmount - limpeza
-  //     document.querySelector('h1')?.removeEventListener('click', eventFn);
-  //   };
-  // }, []);
-
-  // ComponentDidMount - executa toda vez que dependência mudar
-  // useEffect(() => {
-  //   console.log('C1: ', counter, 'C2:', counter2);
-  // }, [counter, counter2]);
-
-  // return (
-  //   <div>
-  //     <p>
-  //       C1: {counter}
-  //       C2: {counter2}
-  //     </p>
-  //     <h1>Contador: {counter}</h1>
-  //     <button onClick={() => setCounter(counter + 1)}>+</button>
-  //     <button onClick={() => setCounter2(counter2 + 1)}>+[2]</button>
-  //   </div>
-  // );
 
   return (
-    <div>
-      <p>Teste</p>
-      <h1>C1: {counter}</h1>
-      <Button incrementButton={incrementCounter}>+</Button>
+    <div className="App">
+      <p>
+        <input
+          type="search"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+      </p>
+      {useMemo(() => {
+        return (
+          posts.length > 0 &&
+          posts.map((post) => {
+            return <Post key={post.id} post={post} />;
+          })
+        );
+      }, [posts])}
+      {posts.length <= 0 && <p>Ainda não existem posts.</p>}
     </div>
   );
 }
